89fdafc93c88b21328854d533b06e05c851d2e4c
tanuj96 <tanujmittal96@gmail.com>
2025-04-30 22:53:08 +0530

    backend db setup config

--- server/Savanager.Api/obj/Debug/net8.0/Savanager.Api.AssemblyInfo.cs
+++ server/Savanager.Api/obj/Debug/net8.0/Savanager.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Savanager.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf363eb43f86bd7e3299d1c0433414304bd846cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc1223a9523f9dc0e81f40953db8a9cfd8658886")]
 [assembly: System.Reflection.AssemblyProductAttribute("Savanager.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Savanager.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
